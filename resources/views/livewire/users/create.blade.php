d0277f04645f3521bdc38d93b24e84d40318124b
leddinlozada223 <leddinlozada@outlook.com>
2024-08-05 14:14:33 -0400

    formulario

--- resources/views/livewire/users/create.blade.php
+++ resources/views/livewire/users/create.blade.php
@@ -4,17 +4,17 @@
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 <div>
                     <form class="mt-5  mx-10 container-md  text-center  flex items-center justify-center flex-wrap">
-                        <div class="mt-5 w-full  px-3">
+                        <div class="mt-5 w-full sm:w-72  px-3">
                             <x-label for="name" value="{{ __('Cedula de Identidad') }}" />
-                            <x-input id="name" class="block mt-1 w-full" type="text" name="Cedula de Identidad" :value="old('name')" required autofocus autocomplete="Cedula de Identidad" />
+                            <x-input id="name" class="block mt-1 w-full truncate" type="text" name="Cedula de Identidad" :value="old('name')" required autofocus autocomplete="Cedula de Identidad" />
                         </div>
                         <div class="mt-5 w-full px-3 sm:w-1/2">
                             <x-label for="name" value="{{ __('Name') }}" />
-                            <x-input id="name" class="block mt-1 w-full" type="text" name="Nombre" :value="old('name')" required autofocus autocomplete="name" />
+                            <x-input id="name" class="block mt-1 w-full truncate" type="text" name="Nombre" :value="old('name')" required autofocus autocomplete="name" />
                         </div>
                         <div class="mt-5 w-full px-3 sm:w-1/2">
                             <x-label for="name" value="{{ __('Apellido') }}" />
-                            <x-input id="name" class="block mt-1 w-full" type="text" name="Apellido" :value="old('name')" required autofocus autocomplete="name" />
+                            <x-input id="name" class="block mt-1 w-full truncate" type="text" name="Apellido" :value="old('name')" required autofocus autocomplete="name" />
                         </div>
                         <div class="mt-5 w-full px-3 sm:w-1/2">
                             <x-label for="name" value="{{ __('Fecha de Nacimiento') }}" />
@@ -29,9 +29,9 @@
                                 :values="$roles"
                             />
                         </div>
-                        <div class="mt-5 w-full px-3">
+                        <div class="mt-5  w-full sm:w-72 px-3">
                             <x-label for="name" value="{{ __('Correo Electronico') }}" />
-                            <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+                            <x-input id="name" class="block mt-1 w-full truncate" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
                         </div>
                         <div class="mt-5 mb-5 w-full px-3 sm:w-1/2">
                             <x-label for="password" value="{{ __('Password') }}" />
