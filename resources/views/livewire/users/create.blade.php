2ae55008523b0cab05e75441f79d561a3f371d94
Mauricio Salazar <53304693+ChronosAS@users.noreply.github.com>
2024-08-05 14:47:28 -0400

    merge

--- resources/views/livewire/users/create.blade.php
+++ resources/views/livewire/users/create.blade.php
@@ -2,51 +2,56 @@
     <div class="py-12">
         <div class="max-w-2xl mx-auto sm:px-6 lg:px-8 justify-self-center">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <div>
-                    <form class="mt-5  mx-10 container-md  text-center  flex items-center justify-center flex-wrap">
-                        <div class="mt-5 w-full sm:w-72  px-3">
-                            <x-label for="name" value="{{ __('Cedula de Identidad') }}" />
-                            <x-input id="name" class="block mt-1 w-full truncate" type="text" name="Cedula de Identidad" :value="old('name')" required autofocus autocomplete="Cedula de Identidad" />
-                        </div>
-                        <div class="mt-5 w-full px-3 sm:w-1/2">
-                            <x-label for="name" value="{{ __('Name') }}" />
-                            <x-input id="name" class="block mt-1 w-full truncate" type="text" name="Nombre" :value="old('name')" required autofocus autocomplete="name" />
-                        </div>
-                        <div class="mt-5 w-full px-3 sm:w-1/2">
-                            <x-label for="name" value="{{ __('Apellido') }}" />
-                            <x-input id="name" class="block mt-1 w-full truncate" type="text" name="Apellido" :value="old('name')" required autofocus autocomplete="name" />
-                        </div>
-                        <div class="mt-5 w-full px-3 sm:w-1/2">
-                            <x-label for="name" value="{{ __('Fecha de Nacimiento') }}" />
-                            <x-input id="name" class="block mt-1 w-full" type="date" name="name" :value="old('name')" required autofocus autocomplete="name" />
-                        </div>
-                        <div class=" mt-5  px-3 sm:w-1/2">
-                        <x-label for="name" value="{{ __('Rol') }}" />
-                            <x-select
-                                name="role"
-                                wire="live"
-                                placeholder="Estado"
-                                :values="$roles"
-                            />
-                        </div>
-                        <div class="mt-5  w-full sm:w-72 px-3">
-                            <x-label for="name" value="{{ __('Correo Electronico') }}" />
-                            <x-input id="name" class="block mt-1 w-full truncate" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-                        </div>
-                        <div class="mt-5 mb-5 w-full px-3 sm:w-1/2">
-                            <x-label for="password" value="{{ __('Password') }}" />
-                            <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
-                        </div>
+                <form class="mt-5  mx-10 container-md  text-center  flex items-center justify-center flex-wrap">
+                    <div class="mt-5 w-full sm:w-72 px-3">
+                        <x-label for="document" value="Cedula de Identidad" />
+                        <x-input id="document" class="block mt-1 w-full truncate" type="text" name="document" :value="old('document')" required autocomplete="document" />
+                        <x-input-error for="document"/>
+                    </div>
+                    <div class="mt-5 w-full px-3 sm:w-1/2">
+                        <x-label for="first_names" value="Nombres" />
+                        <x-input id="first_names" class="block mt-1 w-full truncate" type="text" name="first_names" :value="old('first_names')" required autocomplete="first_names" />
+                        <x-input-error for="first_names"/>
+                    </div>
+                    <div class="mt-5 w-full px-3 sm:w-1/2">
+                        <x-label for="last_names" value="Apellidos" />
+                        <x-input id="last_names" class="block mt-1 w-full truncate" type="text" name="last_names" :value="old('last_names')" required autocomplete="last_names" />
+                        <x-input-error for="last_names"/>
+                    </div>
+                    <div class="mt-5 w-full px-3 sm:w-1/2">
+                        <x-label for="dob" value="Fecha de Nacimiento" />
+                        <x-input id="dob" class="block mt-1 w-full" type="date" name="dob" :value="old('dob')" required autocomplete="dob" />
+                        <x-input-error for="dob"/>
+                    </div>
+                    <div class=" mt-5 w-full px-3 sm:w-1/2">
+                        <x-label for="name" value="Rol" />
+                        <x-select
+                            name="role"
+                            wire="live"
+                            placeholder="Estado"
+                            :values="$roles"
+                        />
+                    </div>
+                    <div class="mt-5  w-full sm:w-72 px-3">
+                        <x-label for="email" value="Correo Electronico" />
+                        <x-input id="email" class="block mt-1 w-full truncate" type="text" name="email" :value="old('email')" required autocomplete="email" />
+                        <x-input-error for="email"/>
+                    </div>
+                    <div class="mt-5 mb-5 w-full px-3 sm:w-1/2">
+                        <x-label for="password" value="{{ __('Password') }}" />
+                        <x-input id="password" class="block mt-1 w-full" type="password" name="password" required/>
+                        <x-input-error for="password"/>
+                    </div>
 
-                        <div class="mt-5 mb-5 w-full px-3 sm:w-1/2">
-                            <x-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
-                            <x-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
-                        </div>
-                        <x-button class="ms-4  mb-5 bg-blue-900">
-                           Agregar
-                        </x-button>
-                    </form>
-                </div>
+                    <div class="mt-5 mb-5 w-full px-3 sm:w-1/2">
+                        <x-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
+                        <x-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required/>
+                        <x-input-error for="document"/>
+                    </div>
+                    <x-button class="ms-4  mb-5 bg-blue-900">
+                        Agregar
+                    </x-button>
+                </form>
             </div>
         </div>
     </div>
