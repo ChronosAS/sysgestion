853dd048977100f0f01ebf45ad176f9cf0bfe5ad
leddinlozada223 <leddinlozada@outlook.com>
2024-08-06 11:25:50 -0400

    show ojo

--- resources/views/livewire/users/create.blade.php
+++ resources/views/livewire/users/create.blade.php
@@ -39,9 +39,9 @@
                     </div>
                     <div class="mt-5 w-full px-3 sm:w-1/2">
                         <x-label for="password" value="{{ __('Password') }}" />
-                            <div class="flex flex-col max-w-sm w-full">
-                                <div class="relative" x-data="{ show: false }">
-                                    <input class="mt-1 rounded bg-white block w-full border border-gray-300 py-2 px-3 focus:ring text-base" :type="show ? 'text' : 'password'" name="password" autocomplete="off" type="password" value="LookMaaGreatPassw0rd!">
+                            <div class="flex flex-col max-w-sm w-full relative">
+                                <div class="abosulte" x-data="{ show: false }">
+                                    <input class="mt-1 rounded bg-white block w-full border truncate border-gray-300 py-2 px-3 focus:ring text-base" :type="show ? 'text' : 'password'" name="password" autocomplete="off" type="password" value="LookMaaGreatPassw0rd!">
                                     <div class="absolute top-4 right-4 cursor-pointer">
                                         <svg class="h-4 text-gray-700 block" fill="none" @click="show = !show" :class="{'block': !show, 'hidden':show }" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
                                             <path fill="currentColor" d="M572.52 241.4C518.29 135.59 410.93 64 288 64S57.68 135.64 3.48 241.41a32.35 32.35 0 0 0 0 29.19C57.71 376.41 165.07 448 288 448s230.32-71.64 284.52-177.41a32.35 32.35 0 0 0 0-29.19zM288 400a144 144 0 1 1 144-144 143.93 143.93 0 0 1-144 144zm0-240a95.31 95.31 0 0 0-25.31 3.79 47.85 47.85 0 0 1-66.9 66.9A95.78 95.78 0 1 0 288 160z">
