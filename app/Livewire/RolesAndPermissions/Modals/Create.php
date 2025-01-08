b9a8c0191a9e1beba6316f00b94ac4fc4f4d6456
Mauricio Salazar <53304693+ChronosAS@users.noreply.github.com>
2025-01-08 09:13:48 -0400

    User admin module changes

--- app/Livewire/RolesAndPermissions/Modals/Create.php
+++ app/Livewire/RolesAndPermissions/Modals/Create.php
@@ -2,37 +2,28 @@
 
 namespace App\Livewire\RolesAndPermissions\Modals;
 
-use Spatie\Permission\Models\Role;
 use Illuminate\Support\Str;
 use Livewire\Attributes\On;
 use Livewire\Attributes\Validate;
 use Livewire\Component;
 use Spatie\Permission\Models\Permission;
 
-class CreateAndEdit extends Component
+class Create extends Component
 {
-    public $role = null;
     #[Validate('required',message: 'Porfavor ingrese un nombre.')]
     #[Validate('unique:roles',message: 'Ya existe un rol con este nombre.')]
     #[Validate('string',message: 'Formato de nombre invalido.')]
     #[Validate('max:50',message: 'Nombre exede el tamaño maximo de 50 caracteres.')]
     public $name;
     public $permissions = [];
-    public $showRoleModal = false;
+    public $showCreateRoleModal = false;
 
-    #[On('roleModal')]
-    public function toggleModal($role = null)
+    #[On('createRoleModal')]
+    public function toggleModal()
     {
+        $this->reset(['name','permissions']);
 
-        $this->reset(['role','name','permissions']);
-
-        if($role){
-            $this->role = Role::where('guard_name','web')->where('id',$role)->first();
-            $this->name = $this->role->name;
-            $this->permissions = $this->role->permissions->pluck('name');
-        }
-
-        $this->showRoleModal = !$this->showRoleModal;
+        $this->showCreateRoleModal = !$this->showCreateRoleModal;
 
     }
 
@@ -40,19 +31,10 @@ class CreateAndEdit extends Component
     {
         $this->validate();
 
-        if($this->role){
-            $this->role->update(['name'=>$this->name]);
-            $this->role->permissions()->sync($this->permissions);
-        }else{
-            tap(Role::create([
-                'name'=> $this->name,
-                'guard_name' => 'web'
-            ]),function($role){
-                $role->givePermissionTo($this->permissions);
-            });
-        }
+        $this->role->update(['name'=>$this->name]);
+        $this->role->permissions()->sync($this->permissions);
 
-        session()->flash('flash.banner','Rol creado con exito.');
+        session()->flash('flash.banner','Rol editado con exito.');
         session()->flash('flash.bannerStyle','success');
 
         $this->dispatch('roleChanged');
@@ -61,7 +43,7 @@ class CreateAndEdit extends Component
 
     public function render()
     {
-        return view('livewire.roles-and-permissions.modals.create-and-edit',[
+        return view('livewire.roles-and-permissions.modals.create',[
             'permissionsGrouped' => $this->getPermissions()
         ]);
     }
---
+++ app/Livewire/RolesAndPermissions/Modals/Edit.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Livewire\RolesAndPermissions\Modals;
+
+use Spatie\Permission\Models\Role;
+use Illuminate\Support\Str;
+use Livewire\Attributes\On;
+use Livewire\Attributes\Validate;
+use Livewire\Component;
+use Spatie\Permission\Models\Permission;
+
+class Edit extends Component
+{
+    public $role = null;
+    #[Validate('required',message: 'Porfavor ingrese un nombre.')]
+    #[Validate('unique:roles,name,{$this->role->id}',message: 'Ya existe un rol con este nombre.')]
+    #[Validate('string',message: 'Formato de nombre invalido.')]
+    #[Validate('max:50',message: 'Nombre exede el tamaño maximo de 50 caracteres.')]
+    public $name;
+    public $permissions = [];
+    public $showEditRoleModal = false;
+
+    #[On('editRoleModal')]
+    public function toggleModal($role = null)
+    {
+
+        $this->reset(['role','name','permissions']);
+
+        if ($role) {
+            $this->role = Role::where('guard_name', 'web')->where('id', $role)->first();
+            $this->name = $this->role->name;
+            $this->permissions = $this->role->permissions->pluck('id');
+        }
+
+        $this->showEditRoleModal = !$this->showEditRoleModal;
+
+    }
+
+    public function save()
+    {
+        // $this->validate();
+
+        $this->role->update(['name'=>$this->name]);
+        $this->role->permissions()->sync($this->permissions);
+
+        session()->flash('flash.banner','Rol creado con exito.');
+        session()->flash('flash.bannerStyle','success');
+
+        $this->dispatch('roleChanged');
+        $this->toggleModal();
+    }
+
+    public function render()
+    {
+        return view('livewire.roles-and-permissions.modals.edit',[
+            'permissionsGrouped' => $this->getPermissions()
+        ]);
+    }
+
+    private function getPermissions()
+    {
+        return Permission::all()
+            ->map(function ($permission) {
+                $model = (string) Str::of($permission->name)
+                    ->before(':')
+                    ->title();
+
+                $permissionName = (string) Str::of($permission->name)
+                    ->after(':')
+                    ->title();
+
+                return [
+                    'model' => $model,
+                    'permission_id' => $permission->id,
+                    'permission_name' => $permissionName,
+                    'permission_fullname' => $permission->name,
+                ];
+            })
+            ->groupBy('model')
+            ->sortBy('model');
+    }
+}
