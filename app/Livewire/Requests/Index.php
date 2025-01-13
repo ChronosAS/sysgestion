3556eaf3506e3499fa64dddb077166b740a32be9
Mauricio Salazar <53304693+ChronosAS@users.noreply.github.com>
2025-01-13 12:54:25 -0400

    Add layout attribute to Requests index component for improved header display

--- app/Livewire/Requests/Index.php
+++ app/Livewire/Requests/Index.php
@@ -2,10 +2,12 @@
 
 namespace App\Livewire\Requests;
 
+use Livewire\Attributes\Layout;
 use Livewire\Component;
 
 class Index extends Component
 {
+    #[Layout('layouts.app',['header'=>'Solicitudes'])]
     public function render()
     {
         return view('livewire.requests.index');
