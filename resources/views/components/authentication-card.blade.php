45e98f258cced9ce90d2415d534d833034b4cd05
leddinlozada223 <leddinlozada@outlook.com>
2025-01-23 13:52:12 -0400

    Update authentication styles; change background colors and button hover effects for improved accessibility

--- resources/views/components/authentication-card.blade.php
+++ resources/views/components/authentication-card.blade.php
@@ -1,9 +1,9 @@
-<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-blue-800">
+<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-slate-200">
     <div>
         {{ $logo }}
     </div>
 
-    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-blue-600 shadow-md overflow-hidden sm:rounded-lg">
+    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-blue-700 shadow-md overflow-hidden sm:rounded-lg">
         {{ $slot }}
     </div>
 </div>
