b21387c46eef4c9237f0f76217e44d9846576e3e
Mauricio Salazar <53304693+ChronosAS@users.noreply.github.com>
2025-02-04 10:03:41 -0400

    Update authentication card logo and background image in login view for improved branding

--- resources/views/components/authentication-card.blade.php
+++ resources/views/components/authentication-card.blade.php
@@ -1,10 +1,8 @@
-<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-slate-200">
+<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-slate-200 bg-cover bg-center" style="background-image: url('assets/img/platsol.png');">
     <div class="z-50">
         {{ $logo }}
     </div>
-
-    <img class=" z-40 absolute" src="{{ asset('assets/img/platsol.png') }}" alt="logo-uptjaa">
-    <div class=" z-50 w-full sm:max-w-md mt-6 px-6 py-4 bg-transparent/[.05] shadow-md overflow-hidden sm:rounded-lg">
+    <div class="z-50 w-full sm:max-w-md mt-6 px-6 py-4 bg-transparent/[.05] shadow-md overflow-hidden sm:rounded-lg">
         {{ $slot }}
     </div>
 </div>
