cf66c2cedd57e59d09b84456446f61e417758ad6
leddinlozada223 <leddinlozada@outlook.com>
2025-02-11 10:35:14 -0400

    Refactor dashboard slider component for improved functionality and user experience

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,73 +2,89 @@
     
 
     <div class="py-12">
-        <div class="w-[75%] mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg ">
-                <!-- Carousel Start -->
-                <div x-data="carousel()" class="relative">
-                    <div class="flex overflow-hidden w-auto">
-                        <template x-for="(card, index) in cards" :key="index">
-                            <div x-show="currentSlide === Math.floor(index / 3)" class="flex md:w-1/3 sm:w-1/2  p-4 ">
-                                <div class="bg-slate-300 p-4 rounded-lg shadow-lg w-full">
-                                    <img :src="card.image" class=" sm:w-[25rem] sm:h-[23rem] xl:w-[15rem] xl:h-[13rem] md:w-[15rem] md:h-[13rem] 2xl:w-[25rem] 2xl:h-[23rem] mb-4">
-                                    <h2 class="text-sm  font-bold mb-2" x-text="card.title"></h2>
-                                    <p class="text-gray-700 text-sm font-semibold" x-text="card.description"></p>
+                <!-- Slider Start -->
+                <div x-data="Carousel()" class="relative w-full">
+                    <div class="overflow-hidden rounded-lg">
+                        <div class="flex transition-transform duration-500 ease-in-out pb-10" :style="`transform: translateX(-${currentIndex * 100}%)`">
+                            <template x-for="(card, index) in cards" :key="index">
+                                <div  class="w-full flex-shrink-0">
+                                    <div class="bg-slate-300 p-4 rounded-lg shadow-lg w-full">
+                                        <img :src="card.image" class="w-[25rem] h-[20rem] sm:w-[23rem] sm:h-[20rem] xl:w-[30rem] xl:h-[24rem] md:w-[15rem] md:h-[13rem] 2xl:w-[30rem] 2xl:h-[24rem] mb-4">
+                                        <h2 class="text-sm font-bold mb-2" x-text="card.title"></h2>
+                                        <p class="text-gray-700 text-sm font-semibold" x-text="card.description"></p>
+                                    </div>
                                 </div>
-                            </div>
+                            </template>
+                        </div>
+                    </div>
+                    <div class="absolute bottom-4 left-0 right-0 flex justify-center space-x-2 pt-5">
+                        <template x-for="index in cards.length" :key="index">
+                            <button class="rounded-full w-3 h-3 focus:outline-none" :class="{ 'bg-blue-500': currentIndex === index - 1, 'bg-gray-300': currentIndex !== index - 1 }" @click="goToSlide(index - 1)">
+                            
+                            </button>
                         </template>
                     </div>
-                    <button @click="currentSlide = (currentSlide - 1 + Math.ceil(cards.length / 3)) % Math.ceil(cards.length / 3)"  class="  absolute  left-0 top-1/2 transform -translate-y-1/2  -px-6 py-1  ">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-10">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
-                        </svg>
-                    </button>
-                    <button @click="currentSlide = (currentSlide + 1) % Math.ceil(cards.length / 3)"  class="absolute  right-0  top-1/2 transform -translate-y-1/2  -px-6 py-1  ">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-10">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
-                        </svg>
-                    </button>
                 </div>
-                <script>
-                    document.addEventListener('alpine:init', () => {
-                        Alpine.data('carousel', () => ({
-                            cards: [
-                                {
-                                    image: 'assets/img/foto1.webp',
-                                    title: 'La dirección de Gestión Social de la Alcaldía de Lechería celebró el cumpleaños de los adultos mayores que forman parte del programa "Abuelos de Lechería".',
-                                    description: 'En esta oportunidad, más de 60 abuelos fueron agasajados en una fiesta que se realiza bimensualmente y que se ha convertido en una tradición llena de alegría.'
-                                },
-                                {
-                                    image: 'assets/img/foto2.webp',
-                                    title: 'La plaza "El Parque" se convirtió en un espacio de paz y armonía gracias a la actividad "Yoga Para Abuelos", organizada por la dirección de Gestión Social de la Alcaldía de Lechería.',
-                                    description: 'El evento fue una ocasión especial para que los presentes disfrutaran de un ambiente festivo y compartieran momentos inolvidables.'
-                                },
-                                {
-                                    image: 'assets/img/foto3.webp',
-                                    title: 'Más de 70 adultos mayores del programa “Abuelos de Lechería”, de la dirección Gestión Social de la Alcaldía de Lechería, tuvieron una mañana entretenida en las instalaciones del Centro de formación teatral, Puertoteatro el pasado jueves 3 de octubre. ',
-                                    description: 'Para la gestión del alcalde de Lechería Manuel Ferreira esta actividad tiene el compromiso de servir a los adultos mayores y promover el envejecimiento activo y saludable a través de diversas actividades recreativas, culturales y sociales.'
-                                },
-                                {
-                                    image: 'assets/img/foto4.webp',
-                                    title: 'Nuestro programa de “visitas domiciliarias” se mantiene en este 2025, brindando atención médica y cuidados especiales a nuestros “Abuelos de Lechería”.',
-                                    description: 'Cada jueves por la mañana, de la mano de la doctora de la Clínica Municipal de Lechería (Imasur) y voluntaria social de la gestión, Jennis Barrera, se realizan chequeos médicos de rutina para constatar la salud de los adultos mayores pertenecientes al programa “Abuelos de Lechería”.'
-                                },
-                                {
-                                    image: 'assets/img/foto5.webp',
-                                    title: 'Con mucho cariño, celebramos nuestro compartir navideño para los abuelitos de nuestro programa. Como cada año, disfrutamos del plato navideño y música increíble.',
-                                    description: 'Además de ofrecer atención médica, se dio inicio al compartir navideño para el programa "Abuelos de Lechería".'
-                                },
-                                {
-                                    image: 'assets/img/foto6.webp',
-                                    title: 'La dirección Gestión Social de la Alcaldía de Lechería incluyó atención psicológica en visitas domiciliarias a “Abuelos de Lechería”',
-                                    description: 'Cada jueves por la mañana, de la mano de la doctora de la Clínica Municipal de Lechería (Imasur) y voluntaria social de la gestión, Jennis Barrera, se realizan chequeos médicos de rutina para constatar la salud de los adultos mayores pertenecientes al programa “Abuelos de Lechería”.'
-                                }
-                            ],
-                            currentSlide: 0
-                        }))
-                    })
-                </script>
                 <!-- Carousel End -->
             </div>
         </div>
     </div>
+    <script>
+        document.addEventListener('alpine:init', () => {
+            Alpine.data('Carousel', () => ({
+                cards: [
+                    {
+                        image: 'assets/img/foto1.webp',
+                        title: 'La dirección de Gestión Social de la Alcaldía de Lechería celebró el cumpleaños de los adultos mayores que forman parte del programa "Abuelos de Lechería".',
+                        description: 'En esta oportunidad, más de 60 abuelos fueron agasajados en una fiesta que se realiza bimensualmente y que se ha convertido en una tradición llena de alegría.'
+                    },
+                    {
+                        image: 'assets/img/foto2.webp',
+                        title: 'La plaza "El Parque" se convirtió en un espacio de paz y armonía gracias a la actividad "Yoga Para Abuelos", organizada por la dirección de Gestión Social de la Alcaldía de Lechería.',
+                        description: 'El evento fue una ocasión especial para que los presentes disfrutaran de un ambiente festivo y compartieran momentos inolvidables.'
+                    },
+                    {
+                        image: 'assets/img/foto3.webp',
+                        title: 'Más de 70 adultos mayores del programa “Abuelos de Lechería”, de la dirección Gestión Social de la Alcaldía de Lechería, tuvieron una mañana entretenida en las instalaciones del Centro de formación teatral, Puertoteatro el pasado jueves 3 de octubre. ',
+                        description: 'Para la gestión del alcalde de Lechería Manuel Ferreira esta actividad tiene el compromiso de servir a los adultos mayores y promover el envejecimiento activo y saludable a través de diversas actividades recreativas, culturales y sociales.'
+                    },
+                    {
+                        image: 'assets/img/foto4.webp',
+                        title: 'Nuestro programa de “visitas domiciliarias” se mantiene en este 2025, brindando atención médica y cuidados especiales a nuestros “Abuelos de Lechería”.',
+                        description: 'Cada jueves por la mañana, de la mano de la doctora de la Clínica Municipal de Lechería (Imasur) y voluntaria social de la gestión, Jennis Barrera, se realizan chequeos médicos de rutina para constatar la salud de los adultos mayores pertenecientes al programa “Abuelos de Lechería”.'
+                    },
+                    {
+                        image: 'assets/img/foto5.webp',
+                        title: 'Con mucho cariño, celebramos nuestro compartir navideño para los abuelitos de nuestro programa. Como cada año, disfrutamos del plato navideño y música increíble.',
+                        description: 'Además de ofrecer atención médica, se dio inicio al compartir navideño para el programa "Abuelos de Lechería".'
+                    },
+                    {
+                        image: 'assets/img/foto6.webp',
+                        title: 'La dirección Gestión Social de la Alcaldía de Lechería incluyó atención psicológica en visitas domiciliarias a “Abuelos de Lechería”',
+                        description: 'Cada jueves por la mañana, de la mano de la doctora de la Clínica Municipal de Lechería (Imasur) y voluntaria social de la gestión, Jennis Barrera, se realizan chequeos médicos de rutina para constatar la salud de los adultos mayores pertenecientes al programa “Abuelos de Lechería”.'
+                    }
+                ],
+                currentIndex: 0,
+                interval: null,
+                init() {
+                    this.startCarousel();
+                },
+                startCarousel() {
+                    this.interval = setInterval(() => {
+                        this.currentIndex = (this.currentIndex + 1) % this.cards.length;
+                    }, 5000);
+                },
+                stopCarousel() {
+                    clearInterval(this.interval);
+                },
+                goToSlide(index) {
+                    this.currentIndex = index;
+                    this.stopCarousel();
+                    this.startCarousel();
+                }
+            }))
+        })
+    </script>
 </x-app-layout>
