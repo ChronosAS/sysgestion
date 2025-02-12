0095833185a7ee7576643647db6e5e811ba34983
leddinlozada223 <leddinlozada@outlook.com>
2025-02-12 08:10:15 -0400

    Refactor Elder Program edit view to separate first and last names, enhance layout, and update show view headers for clarity

--- resources/views/livewire/elder-program/show.blade.php
+++ resources/views/livewire/elder-program/show.blade.php
@@ -11,7 +11,7 @@
         </div>
         <div class="container mx-auto p-4">
             <div class="bg-white shadow-md rounded-lg p-6">
-                <h2 class="text-2xl font-semibold text-gray-800 mb-4">Información del funcionario</h2>
+                <h2 class="text-2xl font-semibold text-gray-800 mb-4">Información del Abuelo</h2>
                 <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
                     <div class="bg-blue-200 border-l-4 border-blue-500 p-4">
                         <label class="block text-md font-bold text-gray-800">Cédula de identidad</label>
@@ -30,7 +30,7 @@
                         <p class="mt-1 text-gray-900 text-sm"></p>
                     </div>
                     <div class="bg-blue-200 border-l-4 border-blue-500 p-4">
-                        <label class="block text-md font-bold text-gray-800">Sexo</label>
+                        <label class="block text-md font-bold text-gray-800">Edad</label>
                         <p class="mt-1 text-gray-900 text-sm"></p>
                     </div>
                     <div class="bg-blue-200 border-l-4 border-blue-500 p-4">
