5557b06fdc2b87f205345e1701f64d51ee50efc3
leddinlozada223 <leddinlozada@outlook.com>
2025-02-12 10:51:51 -0400

    Add search-select component for civil status and update Elder Program index layout for improved usability

---
+++ resources/views/components/search-select.blade.php
@@ -0,0 +1,52 @@
+<div class="mt-5 w-full px-3 sm:w-1/6" 
+    x-data="{
+        open: false,
+        search: '',
+        options: [
+            { id: 'casado', name: 'Casado' },
+            { id: 'soltero', name: 'Soltero' },
+            { id: 'divorciado', name: 'Divorciado' },
+            { id: 'viudo', name: 'Viudo' },
+        ],
+        selected: '',
+        get filteredOptions() {
+            return this.options.filter(option =>
+                option.name.toLowerCase().includes(this.search.toLowerCase())
+            );
+        },
+        selectOption(option) {
+            this.selected = option.name;
+            @this.set('civil', option.id); // Update Livewire property
+            this.open = false;
+            this.search = ''; // Clear search after selection
+        }
+    }" @click.away="open = false">
+    <x-label for="civil" value="Select" class="block text-sm font-medium text-black"/>
+
+    <div class="relative">
+        <button type="button" @click="open = !open" class="relative mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm rounded-md text-left bg-white cursor-default">
+            <span x-text="selected ? selected : 'Seleccionar'"></span>
+            <span class="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+                <svg class="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                    <path fill-rule="evenodd" d="M10 3a1 1 0 01.707.293l3 3a1 1 0 01-1.414 1.414L10 5.414 7.707 7.707a1 1 0 01-1.414-1.414l3-3A1 1 0 0110 3zm-3.707 9.293a1 1 0 011.414 0L10 14.586l2.293-2.293a1 1 0 011.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clip-rule="evenodd" />
+                </svg>
+            </span>
+        </button>
+
+        <div x-show="open" class="absolute z-10 mt-1 w-full rounded-md bg-white shadow-lg overflow-auto focus:outline-none sm:text-sm" style="display: none;">
+            <div class="py-1">
+                <div class="px-2 pb-2">
+                    <input type="text" x-model="search" class="mt-1 block w-full pl-3 pr-3 py-2 text-gray-900 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 sm:text-sm rounded-md" placeholder="Buscar...">
+                </div>
+                <template x-for="option in filteredOptions" :key="option.id">
+                    <button type="button" @click="selectOption(option)" class="text-gray-900 block w-full text-left px-4 py-2 hover:bg-gray-100 hover:text-gray-700 focus:outline-none focus:bg-gray-100 focus:text-gray-700">
+                        <span x-text="option.name"></span>
+                    </button>
+                </template>
+                <div x-show="filteredOptions.length === 0" class="text-gray-500 px-4 py-2">
+                    No se encontraron opciones.
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/livewire/elder-program/index.blade.php
+++ resources/views/livewire/elder-program/index.blade.php
@@ -1,11 +1,11 @@
 <div>
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-[95rem] mx-auto px-auto sm:px-6 lg:px-8">
             <div class="bg-blue-800 overflow-hidden shadow-xl sm:rounded-lg">
                 <header class="text-center text-xl mt-5 font-black text-white font-sans pb-5 ">Solicitud de Ingreso al Programa Abuelos de Lecheria</header>
                 <div class="p-6 lg:p-8 bg-gray-200 border-t-2  border-blue-700">
                     <div class="relative overflow-x-auto  sm:rounded-lg">
-                        <div class="flex flex-column sm:flex-row flex-wrap space-y-4 sm:space-y-0 items-center justify-between pb-4">
+                        <div class="flex flex-column  space-y-4 sm:space-y-0 items-center justify-between pb-4">
                             {{-- <div class="flex space-x-5 ">
                                  <x-select
                                     name="gender"
@@ -22,10 +22,10 @@
                                 <input wire:model.live='search' type="text" name="search" id="search" class="block p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500" placeholder="Buscar...">
                             </div>
                         </div>
-                        <table class="w-full border border-blue-700   text-right rtl:text-right text-white">
-                            <thead class="text-sm text-white uppercase bg-blue-800">
+                        <table class="w-full border border-blue-700  text-left rtl:text-right text-white">
+                            <thead class="text-sm font-semibold  text-white uppercase bg-blue-800">
                                 <tr>
-                                    <th scope="col" class="px-4 py-3">
+                                    <th scope="col" class="px-10 py-3 ">
                                         <div class="flex   items-center  ">
                                             Cédula
                                             <a href="#" wire:click.prevent="sortBy('document')" ><svg class="w-3 h-3 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
@@ -33,15 +33,17 @@
                                                 </svg></a>
                                         </div>
                                     </th>
-                                    <th scope="col" class="px-4 py-3">
-                                        <div class="flex   items-center ">
+                                    <th scope="col" class="px-10 py-3 ">
+                                        <div class="flex text-wrap  items-center ">
                                             Nombre Completo
-                                            <a href="#" wire:click.prevent="sortBy('')"><svg class="w-3 h-3 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
-                                                <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z"/>
-                                                </svg></a>
+                                            <a href="#" wire:click.prevent="sortBy('')">
+                                                <svg class="w-3 h-3 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                                                    <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z"/>
+                                                </svg>
+                                            </a>
                                         </div>
                                     </th>
-                                    <th scope="col" class="px-4 py-3">
+                                    <th scope="col" class="px-10 py-3">
                                         <div class="flex   items-center ">
                                             Edad
                                             <a href="#" wire:click.prevent="sortBy('')"><svg class="w-3 h-3 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
@@ -49,15 +51,15 @@
                                                 </svg></a>
                                         </div>
                                     </th>
-                                    <th scope="col" class="px-4 py-3">
+                                    <th scope="col" class="px-10 py-3">
                                         <div class="flex   items-center ">
-                                            Correo
+                                            Correo Electrónico
                                             <a href="#" wire:click.prevent="sortBy('email')"><svg class="w-3 h-3 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
                                                 <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z"/>
                                                 </svg></a>
                                         </div>
                                     </th>
-                                    <th scope="col" class="px-4 py-3">
+                                    <th scope="col" class="px-10 py-3">
                                         <div class="flex   items-center ">
                                             Teléfono
                                             <a href="#" wire:click.prevent="sortBy('phone_number')"><svg class="w-3 h-3 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
@@ -65,7 +67,7 @@
                                                 </svg></a>
                                         </div>
                                     </th>
-                                    <th scope="col" class="px-4 py-3">
+                                    <th scope="col" class="px-10 py-3">
                                         <div class="flex   items-center ">
                                             Fecha de Solicitud
                                             <a href="#" wire:click.prevent="sortBy('')"><svg class="w-3 h-3 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
@@ -73,7 +75,7 @@
                                                 </svg></a>
                                         </div>
                                     </th>
-                                    <th scope="col" class="px-4 py-3">
+                                    <th scope="col" class="px-10 py-3">
                                         <div class="flex   items-center ">
                                             Estado
                                             <a href="#" wire:click.prevent="sortBy('gender')"><svg class="w-3 h-3 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
@@ -81,7 +83,7 @@
                                                 </svg></a>
                                         </div>
                                     </th>
-                                    <th scope="col" class="px-4 py-3">
+                                    <th scope="col" class="px-10 py-3">
                                         @can('official:create')
                                             <x-button-href href="{{ route('elder-program.create') }}" class="bg-green-600 hover:bg-green-500">
                                                 Crear
