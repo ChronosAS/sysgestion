311703d023e658daf528b1323f08bf5f1f008161
leddinlozada223 <leddinlozada@outlook.com>
2025-02-21 10:56:31 -0400

    Enhance layout and styling for Elder Program view; improve section organization and visual hierarchy

--- resources/views/livewire/elder-program/show.blade.php
+++ resources/views/livewire/elder-program/show.blade.php
@@ -1,6 +1,6 @@
 <div>
-    <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8 shadow-2xl rounded-xl">
-        <div class="bg-white overflow-hidden  p-6">
+    <div class="max-w-7xl bg-gray-200 mb-6 mx-auto py-6 sm:px-6 lg:px-8 shadow-lg rounded-xl">
+        <div class=" overflow-hidden  p-6">
             <div class="flex justify-between items-center">
                 <h2 class="text-2xl font-semibold leading-tight text-gray-800"></h2>
                 <div>
@@ -8,49 +8,50 @@
                 </div>
             </div>
         </div>
-        <div class="container mx-auto p-4 ">
-            <div class=" ">
-                <h2 class="text-2xl font-semibold text-gray-800 mb-4 text-justify">Información del Abuelo</h2>
-                <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
+    </div>
+    <div class="max-w-7xl bg-gray-200 mx-auto my-6 py-6 sm:px-6 lg:px-8 shadow-lg  rounded-xl">
+        <div class="container mx-auto p-4 "> 
+            <h2 class="text-2xl font-semibold text-gray-800 mb-4 text-center">Información del Abuelo</h2>
+            <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
+                <div class="bg-blue-200 border-l-4 border-blue-500 p-4">
+                    <label class="block text-md font-bold text-gray-800">Cédula de identidad</label>
+                    <p class="mt-1 text-gray-900 text-sm">{{ $elderProgramApplication->elder->document }}</p>
+                </div>
+                <div class="bg-blue-200 border-l-4 border-blue-500 p-4">
+                    <label class="block text-md font-bold text-gray-800">Nombres</label>
+                    <p class="mt-1 text-gray-900 text-sm">{{ $elderProgramApplication->elder->first_names }}</p>
+                </div>
+                <div class="bg-blue-200 border-l-4 border-blue-500 p-4">
+                    <label class="block text-md font-bold text-gray-800">Apellidos</label>
+                    <p class="mt-1 text-gray-900 text-sm">{{ $elderProgramApplication->elder->last_names }}</p>
+                </div>
                     <div class="bg-blue-200 border-l-4 border-blue-500 p-4">
-                        <label class="block text-md font-bold text-gray-800">Cédula de identidad</label>
-                        <p class="mt-1 text-gray-900 text-sm">{{ $elderProgramApplication->elder->document }}</p>
-                    </div>
-                    <div class="bg-blue-200 border-l-4 border-blue-500 p-4">
-                        <label class="block text-md font-bold text-gray-800">Nombres</label>
-                        <p class="mt-1 text-gray-900 text-sm">{{ $elderProgramApplication->elder->first_names }}</p>
-                    </div>
-                    <div class="bg-blue-200 border-l-4 border-blue-500 p-4">
-                        <label class="block text-md font-bold text-gray-800">Apellidos</label>
-                        <p class="mt-1 text-gray-900 text-sm">{{ $elderProgramApplication->elder->last_names }}</p>
-                    </div>
-                     <div class="bg-blue-200 border-l-4 border-blue-500 p-4">
-                        <label class="block text-md font-bold text-gray-800">Fecha de nacimiento</label>
-                        <p class="mt-1 text-gray-900 text-sm">{{ $elderProgramApplication->elder->dob }}</p>
-                    </div>
-                    <div class="bg-blue-200 border-l-4 border-blue-500 p-4">
-                        <label class="block text-md font-bold text-gray-800">Edad</label>
-                        <p class="mt-1 text-gray-900 text-sm">{{ \Carbon\Carbon::parse($elderProgramApplication->elder->dob)->age }}</p>
-                    </div>
-                    <div class="bg-blue-200 border-l-4 border-blue-500 p-4">
-                        <label class="block text-md font-bold text-gray-800">Correo electrónico</label>
-                        <p class="mt-1 text-gray-900 text-sm">{{ $elderProgramApplication->elder->email }}</p>
-                    </div>
-                    <div class="bg-blue-200 border-l-4 border-blue-500 p-4">
-                        <label class="block text-md font-bold text-gray-800">Numero de Telefono</label>
-                        <p class="mt-1 text-gray-900 text-sm">{{ $elderProgramApplication->elder->phone_number }}</p>
-                    </div>
-                    <div class="bg-blue-200 border-l-4 border-blue-500 p-4">
-                        <label class="block text-md font-bold text-gray-800">Dirección</label>
-                        <p class="mt-1 text-gray-900 text-sm">{{ $elderProgramApplication->elder->address }}</p>
-                    </div>
+                    <label class="block text-md font-bold text-gray-800">Fecha de nacimiento</label>
+                    <p class="mt-1 text-gray-900 text-sm">{{ $elderProgramApplication->elder->dob }}</p>
+                </div>
+                <div class="bg-blue-200 border-l-4 border-blue-500 p-4">
+                    <label class="block text-md font-bold text-gray-800">Edad</label>
+                    <p class="mt-1 text-gray-900 text-sm">{{ \Carbon\Carbon::parse($elderProgramApplication->elder->dob)->age }}</p>
+                </div>
+                <div class="bg-blue-200 border-l-4 border-blue-500 p-4">
+                    <label class="block text-md font-bold text-gray-800">Correo electrónico</label>
+                    <p class="mt-1 text-gray-900 text-sm">{{ $elderProgramApplication->elder->email }}</p>
+                </div>
+                <div class="bg-blue-200 border-l-4 border-blue-500 p-4">
+                    <label class="block text-md font-bold text-gray-800">Numero de Telefono</label>
+                    <p class="mt-1 text-gray-900 text-sm">{{ $elderProgramApplication->elder->phone_number }}</p>
+                </div>
+                <div class="bg-blue-200 border-l-4 border-blue-500 p-4">
+                    <label class="block text-md font-bold text-gray-800">Dirección</label>
+                    <p class="mt-1 text-gray-900 text-sm">{{ $elderProgramApplication->elder->address }}</p>
                 </div>
             </div>
+        
         </div>
         <div class="container mx-auto p-4 ">
             <div class="overflow-x-auto">
                 <h1  class="text-2xl font-semibold text-gray-800 mb-4 text-center">Grupo Familiar</h1>
-                <table class="min-w-full bg-white rounded-lg shadow-md">
+                <table class="min-w-full bg-white rounded-lg shadow-md border border-blue-700 ">
                     <thead class="bg-blue-700">
                         <tr>
                             <th class="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider">
@@ -70,7 +71,7 @@
                             </th>
                         </tr>
                     </thead>
-                    <tbody class="bg-slate-300 divide-y divide-gray-200">
+                    <tbody class="bg-white divide-y divide-gray-200">
                         <tr>
                             <td class="px-6 py-4 whitespace-nowrap">
                                 <div class="text-sm text-gray-900">{{ $elderProgramApplication->elder->first_names }}</div>
@@ -93,30 +94,30 @@
                 </table>
             </div>
         </div>
+    </div>
+    <div class="max-w-7xl bg-gray-200 mx-auto my-6 py-6 sm:px-6 lg:px-8 shadow-lg  rounded-xl">
         <div class="container mx-auto p-4 ">
-            <div class=" ">
-                <div class="mt-5  mx-10 container-md  text-center  flex items-center justify-center flex-wrap">
-                    <h2 class="text-2xl font-semibold text-gray-800 p-4 text-center mt-5 w-full px-3 sm:w-4/1">Diagnostico del Caso</h2>
-                    <div class=" p-4 mt-5 w-full px-3 sm:w-1/2">
-                        <label class="bg-blue-600 block text-md font-bold text-white">Ingreso Familiar</label>
-                        <p class="bg-slate-300  text-gray-900 text-sm">{{ $elderProgramApplication->elder->document }}</p>
-                    </div>
-                    <div class="  p-4 mt-5 w-full px-3 sm:w-1/2">
-                        <label class="bg-blue-600 block text-md font-bold text-white">Egreso Familiar</label>
-                        <p class="bg-slate-300  text-gray-900 text-sm">{{ $elderProgramApplication->elder->document }}</p>
-                    </div>
-                    <div class=" p-4 mt-5 w-full px-3 sm:w-4/1">
-                        <label class="bg-blue-600 block text-md font-bold text-white">Aspecto Médico</label>
-                        <p class="bg-slate-300  text-gray-900 text-sm">{{ $elderProgramApplication->elder->document }}</p>
-                    </div>
-                    <div class="p-4 mt-5 w-full px-3 sm:w-4/1">
-                        <label class="bg-blue-600 block text-md font-bold text-white">Aspecto Psico-Social</label>
-                        <p class="bg-slate-300  text-gray-900 text-sm">{{ $elderProgramApplication->elder->document }}</p>
-                    </div>
-                    <div class=" p-4 mt-5 w-full px-3 sm:w-4/1">
-                        <label class="bg-blue-600 block text-md font-bold text-white">Aspecto Físico-Ambiental</label>
-                        <p class="bg-slate-300  text-gray-900 text-sm">{{ $elderProgramApplication->elder->document }}</p>
-                    </div>
+            <div class="mt-1  mx-10 container-md  text-center  flex items-center justify-center flex-wrap ">
+                <h2 class="text-2xl font-semibold text-gray-800  text-center w-full  sm:w-4/1">Diagnostico del Caso</h2>
+                <div class=" sm:mr-4 mt-5 w-full sm:w-1/3 border border-blue-700">
+                    <label class="bg-blue-600 block text-md font-bold text-white">Ingreso Familiar</label>
+                    <p class="bg-white  text-gray-900 text-sm">{{ $elderProgramApplication->elder->document }}</p>
+                </div>
+                <div class=" sm:ml-4  mt-5 w-full  sm:w-1/3 border border-blue-700">
+                    <label class="bg-blue-600 block text-md font-bold text-white">Egreso Familiar</label>
+                    <p class="bg-white  text-gray-900 text-sm">{{ $elderProgramApplication->elder->document }}</p>
+                </div>
+                <div class="  mt-5 w-full  sm:w-4/1 border border-blue-700">
+                    <label class="bg-blue-600 block text-md font-bold text-white">Aspecto Médico</label>
+                    <p class="bg-white  text-gray-900 text-sm">{{ $elderProgramApplication->elder->document }}</p>
+                </div>
+                <div class=" mt-5 w-full  sm:w-4/1  border border-blue-700">
+                    <label class="bg-blue-600 block text-md font-bold text-white">Aspecto Psico-Social</label>
+                    <p class="bg-white  text-gray-900 text-sm">{{ $elderProgramApplication->elder->document }}</p>
+                </div>
+                <div class="  mt-5 w-full  sm:w-4/1  border border-blue-700">
+                    <label class="bg-blue-600 block text-md font-bold text-white">Aspecto Físico-Ambiental</label>
+                    <p class="bg-white  text-gray-900 text-sm">{{ $elderProgramApplication->elder->document }}</p>
                 </div>
             </div>
         </div>
