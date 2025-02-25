fe59fa479815990907a51745627c856925ece66e
leddinlozada223 <leddinlozada@outlook.com>
2025-02-25 14:16:30 -0400

    Add new assets and create carnet view for Elder Program

---
+++ resources/views/carnet.blade.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Carnet</title>
+     <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+        <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body >
+    <div style="width: 486px; height: 306px; background-image: url('pdf/elder-program/assets/carnet-fondo.png'); " class=" bg-contain relative overflow-hidden">
+        <header class="text-center right-3.5 absolute">
+            <h1 class="text-[11px] font-black">ALCALDÍA DEL MUNICIPIO TURISTICO “EL MORRO” <br> LIC. DIEGO BAUTISTA URBANEJA</h1>
+        </header>
+        <div class="absolute top-1  left-[67.5px] ">
+            <!-- logo de lecheria sobre la foto -->
+            <img  class=" w-[74px] " src="pdf/elder-program/assets/logotipo-lecheria-blanco.png" />
+        </div>
+        <div class="absolute top-16  -left-32">
+        <!-- logo detras del cuadro de la foto -->
+            <img  class=" w-[300px] opacity-25" src="pdf/elder-program/assets/logo-sin-letras.png" />
+        </div>
+        <div class="absolute top-20  left-9">
+        <!-- cuadro de la foto -->
+            <img  class=" w-[135px]" src="pdf/elder-program/assets/cuadro-foto.png" />
+        </div>
+        <div class=" absolute right-12 top-9">
+        <!-- logo de abuelos de lecheria debajo del header de alcaldia -->
+            <img  class=" w-44" src="pdf/elder-program/assets/logo-abuelo-lecheria.png" />
+        </div>
+        <main class=" justify-center items-center absolute top-28 right-32">
+            <div class="text-left space-y-7">
+                <p class="text-[10px] font-[1000] -ml-2 " style=" color: #0c558c;">Nombres:</p>
+                <p class="text-[10px] font-[1000] " style=" color: #0c558c;">Apellidos:</p>
+                <p class="text-[10px] font-[1000] " style=" color: #0c558c;">Cédula de Identidad:</p>
+            </div>
+        </main>
+         <!-- Escudo de lecheria como marca de agua -->
+        <div class=" " >
+            <img class=" absolute top-20 inset-y-0 -right-12 w-[175px] opacity-10 " src="pdf/elder-program/assets/escudo.png"/>
+        </div>
+        <div class="absolute bottom-8 left-10  ">
+            <footer class="text-center  flex  flex-row items-center justify-center space-x-16">
+                <p class="text-xs font-black text-white">Fecha de Expedición:</p>
+                <p class="text-[10px] font-black text-sky-950">Sector:</p>
+            </footer>
+        </div>
+        <div class="absolute bottom-0 right-3">
+        <!-- logo de gestion social -->
+            <img  class="w-14" src="pdf/elder-program/assets/logo-gestion-social.png" />
+        </div>
+    </div>
+   
+
+</body>
+</html>
