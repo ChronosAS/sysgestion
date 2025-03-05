ed90130d405c7ad588163609bc4d74a40ef9b3dd
Mauricio Salazar <53304693+ChronosAS@users.noreply.github.com>
2025-03-05 14:48:03 -0400

    Add GenerateModal Livewire component for pension management

---
+++ app/Livewire/ElderProgram/Pension/GenerateModal.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\ElderProgram\Pension;
+
+use Livewire\Component;
+
+class GenerateModal extends Component
+{
+    public function render()
+    {
+        return view('livewire.elder-program.pension.generate-modal');
+    }
+}
