b0db8e51167befdd778bafb3c2c532e2e1870d70
Mauricio Salazar <53304693+ChronosAS@users.noreply.github.com>
2025-03-07 09:52:21 -0400

    Display account number in elder program member details

--- resources/views/livewire/elder-program/show.blade.php
+++ resources/views/livewire/elder-program/show.blade.php
@@ -80,7 +80,7 @@
                 </div>
                 <div class="bg-blue-200 border-l-4 border-blue-500 p-4">
                     <label class="block text-md font-bold text-gray-800">Número de Cuenta</label>
-                    <p class="mt-1 text-gray-900 text-sm"></p>
+                    <p class="mt-1 text-gray-900 text-sm">{{ $elderProgramMember->account_number }}</p>
                 </div>
             </div>
 
