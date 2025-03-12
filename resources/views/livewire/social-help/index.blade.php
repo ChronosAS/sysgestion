6b38cd63c4f45949061c352e706291d50567d293
leddinlozada223 <leddinlozada@outlook.com>
2025-03-12 12:23:09 -0400

    Update social help views: rename headers and buttons for clarity, and clean up unused code

--- resources/views/livewire/social-help/index.blade.php
+++ resources/views/livewire/social-help/index.blade.php
@@ -2,7 +2,7 @@
     <div class="py-12">
         <div class="max-w-[100rem] mx-auto px-auto sm:px-6 lg:px-8">
             <div class="bg-blue-800 overflow-hidden shadow-xl sm:rounded-lg">
-                <header class="text-center text-xl mt-5 font-black text-white font-sans pb-5 ">Solicitudes de Ayuda Social</header>
+                <header class="text-center text-xl mt-5 font-black text-white font-sans pb-5 ">Solicitudes Medicas</header>
                 <div class="p-6 lg:p-8 bg-gray-200 border-t-2  border-blue-700">
                     <div class="relative overflow-x-auto  sm:rounded-lg">
                         <div class="flex flex-column  space-y-4 sm:space-y-0 items-center justify-between pb-4">
@@ -26,7 +26,7 @@
                                 <div class="mx-4">
                                     
                                         <x-button-href href="{{ route('social-help.create') }}" class="bg-green-600 h-10 hover:bg-green-500">
-                                            Registrar Ayuda
+                                            Registrar Solicitud
                                         </x-button-href>
                                  
                                 </div>
@@ -79,7 +79,7 @@
                                     </th>
                                     <th scope="col" class="px-10 py-3">
                                         <div class="flex  text-wrap items-center ">
-                                            Numero de Cuenta
+                                            Estado
                                             <a href="#" wire:click.prevent="sortBy('account_number')"><svg class="w-3 h-3 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
                                                 <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z"/>
                                                 </svg></a>
@@ -87,20 +87,13 @@
                                     </th>
                                     <th scope="col" class="px-10 py-3">
                                         <div class="flex   items-center ">
-                                            Fecha de ingreso
+                                            Fecha de Solicitud
                                             <a href="#" wire:click.prevent="sortBy('created_at')"><svg class="w-3 h-3 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
                                                 <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z"/>
                                                 </svg></a>
                                         </div>
                                     </th>
-                                    <th scope="col" class="px-10 py-3">
-                                        <div class="flex   items-center ">
-                                            Carnet
-                                            <a href="#" wire:click.prevent="sortBy('has_card')"><svg class="w-3 h-3 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
-                                                <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z"/>
-                                                </svg></a>
-                                        </div>
-                                    </th>
+                                
                                     <th scope="col" class="px-10 py-3">
                                         <div class="flex   items-center ">
 
@@ -110,7 +103,7 @@
                             </thead>
                             <tbody class="">
                                
-                                    <tr class="bg-white border-b">
+                                    {{-- <tr class="bg-white border-b">
                                         <th scope="row" class=" py-4 font-medium text-gray-900 whitespace-nowrap ">
                                             <a href="#" wire:navigate class="text-blue-600 hover:underline hover:text-blue-800"> 
                                             </a>
@@ -133,17 +126,14 @@
                                         <td class=" text-gray-900 py-4">
                                            
                                         </td>
-                                        <td class="py-4">
-                                           
-                                        </td>
                                         <td class=" text-gray-900 py-4">
-                                             <a href="#" wire:navigate class="">
+                                            <a href="#" wire:navigate class="">
                                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6 text-yellow-500 cursor-pointer  hover:text-yellow-600">
                                                     <path  d="M21.731 2.269a2.625 2.625 0 0 0-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 0 0 0-3.712ZM19.513 8.199l-3.712-3.712-12.15 12.15a5.25 5.25 0 0 0-1.32 2.214l-.8 2.685a.75.75 0 0 0 .933.933l2.685-.8a5.25 5.25 0 0 0 2.214-1.32L19.513 8.2Z" />
                                                 </svg>
-                                            </a>
+                                            </a> 
                                         </td>
-                                    </tr>
+                                    </tr> --}}
                         
                                     <tr>
                                         <td class="px-6 py-4 text-center text-xl col-span-5 text-black bg-white" colspan="10">
