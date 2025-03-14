912b3ac96cc3de641d838719479cf81bbad2e605
Mauricio Salazar <53304693+ChronosAS@users.noreply.github.com>
2025-03-14 08:25:24 -0400

    Add donation management functionality: implement create, index, and show components; update validation rules for email uniqueness

---
+++ app/Livewire/Medicines/Donations/Index.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Medicines\Donations;
+
+use Livewire\Component;
+
+class Index extends Component
+{
+    public function render()
+    {
+        return view('livewire.medicines.donations.index');
+    }
+}
---
+++ app/Livewire/Medicines/Donations/Show.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Medicines\Donations;
+
+use Livewire\Component;
+
+class Show extends Component
+{
+    public function render()
+    {
+        return view('livewire.medicines.donations.show');
+    }
+}
