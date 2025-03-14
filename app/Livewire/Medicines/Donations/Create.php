f71091449888e9e5f60eca26f8dbe04f499a5519
Mauricio Salazar <53304693+ChronosAS@users.noreply.github.com>
2025-03-14 08:30:18 -0400

    Add layout attribute to donations components for consistent styling

--- app/Livewire/Medicines/Donations/Create.php
+++ app/Livewire/Medicines/Donations/Create.php
@@ -2,8 +2,10 @@
 
 namespace App\Livewire\Medicines\Donations;
 
+use Livewire\Attributes\Layout;
 use Livewire\Component;
 
+#[Layout('layouts.app')]
 class Create extends Component
 {
     public function render()
--- app/Livewire/Medicines/Donations/Index.php
+++ app/Livewire/Medicines/Donations/Index.php
@@ -2,8 +2,10 @@
 
 namespace App\Livewire\Medicines\Donations;
 
+use Livewire\Attributes\Layout;
 use Livewire\Component;
 
+#[Layout('layouts.app')]
 class Index extends Component
 {
     public function render()
