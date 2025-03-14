8a701b4db32ca22f736c083a811a7c20c8ef20f3
leddinlozada223 <leddinlozada@outlook.com>
2025-03-14 09:00:56 -0400

    Add gender selection to donation creation form and update navigation links

--- app/Livewire/Medicines/Donations/Create.php
+++ app/Livewire/Medicines/Donations/Create.php
@@ -8,6 +8,13 @@ use Livewire\Component;
 #[Layout('layouts.app')]
 class Create extends Component
 {
+    public $genders = [
+        'male' => 'Masculino',
+        'female' => 'Femenino',
+    ];
+
+    public $gender;
+
     public function render()
     {
         return view('livewire.medicines.donations.create');
--- resources/views/livewire/medicines/donations/create.blade.php
+++ resources/views/livewire/medicines/donations/create.blade.php
@@ -21,7 +21,7 @@
                     <div class="mt-5 w-full px-3 sm:w-1/6" >
                         <x-label for="gender" value="Sexo" class="block text-sm font-medium text-black"/>
                         <select wire:model='gender' id="gender" name="gender" class="mt-1 block w-full pl-3 pr-10 py-2 text-base cursor-pointer border-gray-300 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm rounded-md text-center">
-                            <option value="#" class="text-center ">Seleccionar</option>
+                            <option value="" class="text-center">Seleccionar</option>
                             @foreach ($genders as $value => $name)
                                 <option value="{{ $value }}" class="text-center">{{ $name }}</option>
                             @endforeach
@@ -82,7 +82,7 @@
                         <x-input id="observation" wire:model='observation' class="block mt-1 w-full truncate" type="text" name="observation" :value="old('observation')" autocomplete="observation" />
                         <x-input-error class="text-xs" for="observation"/>
                     </div>
-                    <div class=" mt-5 w-full px-3 sm:w-4/4">
+                    {{-- <div class=" mt-5 w-full px-3 sm:w-4/4">
                         <x-table.table class="w-full">
                             <x-slot name="thead">
                                 <tr class="bg-blue-800 text-white ">
@@ -96,39 +96,38 @@
                                         Apellido/s
                                     </x-table.th>
                                     <x-table.th class="pb-3 text-center">
-                                        <button @click="$dispatch('addBeneficiaryModal',{ beneficiaries })" type="button" class="inline-block cursor-pointer rounded-md bg-blue-900 hover:bg-blue-500 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-primary-3 transition duration-150 ease-in-out hover:bg-primary-accent-300 hover:shadow-primary-2 focus:bg-primary-accent-300 focus:shadow-primary-2 focus:outline-none focus:ring-0 active:bg-primary-600 active:shadow-primary-2 motion-reduce:transition-none">Agregar</button>
+                                        <button  type="button" class="inline-block cursor-pointer rounded-md bg-blue-900 hover:bg-blue-500 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-primary-3 transition duration-150 ease-in-out hover:bg-primary-accent-300 hover:shadow-primary-2 focus:bg-primary-accent-300 focus:shadow-primary-2 focus:outline-none focus:ring-0 active:bg-primary-600 active:shadow-primary-2 motion-reduce:transition-none">Agregar</button>
                                     </x-table.th>
                                 </tr>
                             </x-slot>
                             <x-slot name="tbody">
-                                @forelse($beneficiaries as $index => $beneficiary)
-                                    <tr class="bg-white shadow-md border border-gray-700 text-center">
+                              
+                                    {{-- <tr class="bg-white shadow-md border border-gray-700 text-center">
                                         <td class="text-black">
-                                            {{ $beneficiary['document'] }}
+                                           
                                         </td>
                                         <td class="text-black">
-                                            {{ $beneficiary['first_names'] }}
+                                            
                                         </td>
                                         <td class="text-black">
-                                            {{ $beneficiary['last_names'] }}
+                                          
                                        </td>
                                         <x-table.td class="text-center">
-                                            <a @click="$dispatch('editBeneficiaryModal', { index: {{ $index }}, beneficiaries })" class="font-bold text-blue-500 cursor-pointer hover:underline">Editar</a>
-                                            <a wire:confirm='seguro que desea remover este beneficiario?' wire:click="removeBeneficiary({{ $index }})" class="font-bold text-red-500 cursor-pointer hover:underline">Remover</a>
+                                           
                                         </x-table.td>
-                                    </tr>
-                                @empty
+                                    </tr> 
+                                
                                     <tr>
                                         <td colspan="4" class="px-6 py-4 bg-white text-center text-black text-xl">
                                             No hay beneficiarios agregados
                                         </td>
                                     </tr>
-                                @endforelse
+                                
                             </x-slot>
                         </x-table.table>
-                    </div>
+                    </div> --}}
                     <div class="mt-5  mx-10 container-md  text-center  flex items-center justify-center flex-wrap">
-                        <x-button-href href="{{ route('officials.index') }}" class="ms-4 mt-5 mb-5 bg-blue-900 inline-flex items-center px-4 py-2  border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-blue-500 focus:bg-blue-700 active:bg-blue-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-50 transition ease-in-out duration-150">
+                        <x-button-href href="" class="ms-4 mt-5 mb-5 bg-blue-900 inline-flex items-center px-4 py-2  border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-blue-500 focus:bg-blue-700 active:bg-blue-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-50 transition ease-in-out duration-150">
                             Regresar
                         </x-button-href>
                         <x-button class="ms-4 mt-5 mb-5 bg-green-600 hover:bg-green-500">
@@ -139,6 +138,5 @@
             </div>
         </div>
     </div>
-    <livewire:officials.beneficiaries.edit />
-    <livewire:officials.beneficiaries.create :beneficiaries="$beneficiaries"/>
+    
 </div>
