58331b22e5b532a5d361040e6ab864bb62f93a6b
leddinlozada223 <leddinlozada@outlook.com>
2025-03-19 12:03:58 -0400

    Enhance donations view: add donor information section and medication donation table with improved layout and styling

--- resources/views/livewire/medicines/donations/show.blade.php
+++ resources/views/livewire/medicines/donations/show.blade.php
@@ -1,3 +1,142 @@
 <div>
-    {{-- Close your eyes. Count to one. That is how long forever feels. --}}
+    <div class="max-w-7xl bg-gray-200 mx-auto my-6 py-6 sm:px-6 lg:px-8 shadow-lg  rounded-xl">
+        
+        <div class="container mx-auto p-4 ">
+            <h2 class="text-2xl font-semibold text-gray-800 mb-4 text-center">Información del Donante</h2>
+            <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
+                <div class="bg-blue-200 border-l-4 border-blue-500 p-4">
+                    <label class="block text-md font-bold text-gray-800">Cédula de identidad</label>
+                    <p class="mt-1 text-gray-900 text-sm"></p>
+                </div>
+                <div class="bg-blue-200 border-l-4 border-blue-500 p-4">
+                    <label class="block text-md font-bold text-gray-800">Nombres</label>
+                    <p class="mt-1 text-gray-900 text-sm"></p>
+                </div>
+                <div class="bg-blue-200 border-l-4 border-blue-500 p-4">
+                    <label class="block text-md font-bold text-gray-800">Apellidos</label>
+                    <p class="mt-1 text-gray-900 text-sm"></p>
+                </div>
+                    <div class="bg-blue-200 border-l-4 border-blue-500 p-4">
+                    <label class="block text-md font-bold text-gray-800">Fecha de nacimiento</label>
+                    <p class="mt-1 text-gray-900 text-sm"></p>
+                </div>
+                <div class="bg-blue-200 border-l-4 border-blue-500 p-4">
+                    <label class="block text-md font-bold text-gray-800">Edad</label>
+                    <p class="mt-1 text-gray-900 text-sm"></p>
+                </div>
+                <div class="bg-blue-200 border-l-4 border-blue-500 p-4">
+                    <label class="block text-md font-bold text-gray-800">Correo electrónico</label>
+                    <p class="mt-1 text-gray-900 text-sm"></p>
+                </div>
+                <div class="bg-blue-200 border-l-4 border-blue-500 p-4">
+                    <label class="block text-md font-bold text-gray-800">Numero de Telefono</label>
+                    <p class="mt-1 text-gray-900 text-sm"></p>
+                </div>
+                <div class="bg-blue-200 border-l-4 border-blue-500 p-4">
+                    <label class="block text-md font-bold text-gray-800">Grado de Instrucción</label>
+                    <p class="mt-1 text-gray-900 text-sm"></p>
+                </div>
+                <div class="bg-blue-200 border-l-4 border-blue-500 p-4">
+                    <label class="block text-md font-bold text-gray-800">Ocupación</label>
+                    <p class="mt-1 text-gray-900 text-sm"></p>
+                </div>
+                <div class="bg-blue-200 border-l-4 border-blue-500 p-4">
+                    <label class="block text-md font-bold text-gray-800">Edo. Civil</label>
+                    <p class="mt-1 text-gray-900 text-sm"></p>
+                </div>
+                <div class="bg-blue-200 border-l-4 border-blue-500 p-4">
+                    <label class="block text-md font-bold text-gray-800">Estado</label>
+                    <p class="mt-1 text-gray-900 text-sm"></p>
+                </div>
+                <div class="bg-blue-200 border-l-4 border-blue-500 p-4">
+                    <label class="block text-md font-bold text-gray-800">Municipio</label>
+                    <p class="mt-1 text-gray-900 text-sm"></p>
+                </div>
+                <div class="bg-blue-200 border-l-4 border-blue-500 p-4">
+                    <label class="block text-md font-bold text-gray-800">Parroquia</label>
+                    <p class="mt-1 text-gray-900 text-sm"></p>
+                </div>
+                <div class="bg-blue-200 border-l-4 border-blue-500 p-4">
+                    <label class="block text-md font-bold text-gray-800">Dirección</label>
+                    <p class="mt-1 text-gray-900 text-sm"></p>
+                </div>
+                <div class="bg-blue-200 border-l-4 border-blue-500 p-4">
+                    <label class="block text-md font-bold text-gray-800">Observación del Caso</label>
+                    <p class="mt-1 text-gray-900 text-sm"></p>
+                </div>
+            </div>
+
+        </div>
+        <div class="container mx-auto p-4 ">
+            <div class="overflow-x-auto">
+                <h1  class="text-2xl font-semibold text-gray-800 mb-4 text-center">Lista de Medicamentos Donados</h1>
+                <table class="min-w-full bg-white rounded-lg shadow-md border border-blue-700 ">
+                    <thead class="bg-blue-700">
+                        <tr>
+                            <th class="px-6 py-3 text-center text-xs font-medium text-white uppercase tracking-wider">
+                               Nombre Comercial
+                            </th>
+                            <th class="px-6 py-3 text-center text-xs font-medium text-white uppercase tracking-wider">
+                               Componente Activo
+                            </th>
+                            <th class="px-6 py-3 text-center text-xs font-medium text-white uppercase tracking-wider">
+                                Cantidad (Composición)
+                            </th>
+                            <th class="px-6 py-3 text-center text-xs font-medium text-white uppercase tracking-wider">
+                                Laboratorio
+                            </th>
+                            <th class="px-6 py-3 text-center text-xs font-medium text-white uppercase tracking-wider">
+                                Unidades
+                            </th>
+                            <th class="px-6 py-3 text-center text-xs font-medium text-white uppercase tracking-wider">
+                               Fecha de Ingreso
+                            </th>
+                            <th class="px-6 py-3 text-center text-xs font-medium text-white uppercase tracking-wider">
+                                Fecha de Vencimiento
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody class="bg-slate-300 divide-y divide-gray-200">
+                      
+                            <tr>
+                                <td class="px-6 py-4 whitespace-nowrap">
+                                  
+                                </td>
+                                <td class="px-6 py-4 whitespace-nowrap">
+                                    
+                                </td>
+                                <td class="px-6 py-4 whitespace-nowrap">
+                                    
+                                </td>
+                                <td class="px-6 py-4 whitespace-nowrap">
+                                    
+                                </td>
+                                <td class="px-6 py-4 whitespace-nowrap">
+                                   
+                                </td>
+                                <td class="px-6 py-4 whitespace-nowrap">
+                                   
+                                </td>
+                                <td class="px-6 py-4 whitespace-nowrap">
+                                   
+                                </td>
+                            </tr>
+                       
+                        <tr>
+                            <td class="px-6 py-4 text-center text-xl col-span-5 text-black bg-white" colspan="10">
+                                No hay Medicamentos Donados registrados.
+                            </td>
+                        </tr>
+                    
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <div class="flex  justify-around overflow-hidden  space-x-2 text-center">
+            <a href="{{ route('medicines.donations.index') }}" class="bg-blue-600 text-white text-center px-4 py-2 rounded hover:bg-blue-700">Regresar</a>
+            <a href="#" class="bg-green-600 text-white text-center px-4 py-2 rounded hover:bg-blue-700">Imprimir Reporte</a>
+        </div>
+    </div>
+    {{-- <div class="max-w-7xl bg-gray-200 mx-auto my-6 py-6 sm:px-6 lg:px-8 shadow-lg  rounded-xl">
+    </div> --}}
 </div>
