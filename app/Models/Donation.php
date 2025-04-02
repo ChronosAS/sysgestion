fd241485c9d2a5e40dea9eff0952ac6bedf48b77
Mauricio Salazar <53304693+ChronosAS@users.noreply.github.com>
2025-04-02 12:27:07 -0400

    Add Donation model and migration; implement UUIDs, activity logging, and donor information fields

--- app/Models/Donation.php
+++ app/Models/Donation.php
@@ -2,11 +2,72 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Str;
+use Spatie\Activitylog\LogOptions;
 
 class Donation extends Model
 {
     /** @use HasFactory<\Database\Factories\DonationFactory> */
-    use HasFactory;
+    use HasFactory, HasUuids;
+
+    protected $fillable = [
+        'code',
+        'donor_document',
+        'donor_name',
+        'donor_email',
+        'donor_phone_number',
+        'donor_address',
+        'donor_civil_status',
+        'donor_dob',
+        'donor_gender',
+        'estado_id',
+        'municipio_id',
+        'parroquia_id'
+    ];
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+            ->logOnly([
+                'code',
+                'donor_document',
+            ]);
+    }
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($application){
+            $lastApplication = self::orderBy('code', 'desc')->first();
+            if ($lastApplication) {
+                $lastCode = (int) str_replace('DM', '', $lastApplication->code);
+                $newCode = $lastCode + 1;
+            } else {
+                $newCode = 1;
+            }
+            if (empty($application->code)) {
+                $application->code = 'DM' . Str::padLeft($newCode, 5, '0');
+            }
+        });
+    }
+
+    public function estado() : BelongsTo
+    {
+        return $this->belongsTo(Estado::class,'estado_id');
+    }
+
+    public function municipio() : BelongsTo
+    {
+        return $this->belongsTo(Municipio::class,'municipio_id');
+    }
+
+    public function parroquia() : BelongsTo
+    {
+        return $this->belongsTo(Parroquia::class,'parroquia_id');
+    }
 }
--- database/migrations/2025_03_27_132817_create_donations_table.php
+++ database/migrations/2025_03_27_132817_create_donations_table.php
@@ -18,10 +18,10 @@ return new class extends Migration
             $table->string('donor_name')->nullable();
             $table->string('donor_email')->nullable();
             $table->string('donor_phone_number')->nullable();
-            $table->string('gender')->nullable();
+            $table->string('donor_gender')->nullable();
             $table->string('donor_address')->nullable();
             $table->string('donor_civil_status')->nullable();
-            $table->date('dob')->nullable();
+            $table->date('donor_dob')->nullable();
             $table->integer('estado_id')->nullable();
             $table->integer('municipio_id')->nullable();
             $table->integer('parroquia_id')->nullable();
