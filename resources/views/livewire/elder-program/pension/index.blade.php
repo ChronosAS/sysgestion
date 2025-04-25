0d1b72adc854bc0c9564387907f4ff2036da6915
leddinlozada223 <leddinlozada@outlook.com>
2025-04-25 09:55:03 -0400

    Refactor donation and pension views; update button styles and improve table layout for better usability

--- resources/views/livewire/elder-program/pension/index.blade.php
+++ resources/views/livewire/elder-program/pension/index.blade.php
@@ -23,76 +23,84 @@
                             </div>
                         </div>
                         <table class="w-full border border-blue-700  text-center  text-white">
-                            <thead class="text-sm font-semibold  text-white uppercase bg-blue-800 ">
-                                <tr class="flex justify-around space-x-4">
-                                    <th scope="col" class="px-10 py-3 ">
-                                        <div class="flex   items-center">
+                            <thead class="text-sm font-semibold text-white uppercase bg-blue-800">
+                                <tr>
+                                    <th scope="col" class="px-10 py-3 text-center">
+                                        <div class="flex items-center justify-center">
                                             Código
-                                            <a href="#" wire:click.prevent="sortBy('code')" ><svg class="w-3 h-3 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
-                                                <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z"/>
-                                                </svg></a>
-                                        </div>
-                                    </th>
-                                    <th scope="col" class="px-10 py-3 ">
-                                        <div class="flex text-wrap  items-center ">
-                                            Número Total de Abuelos
-                                            <a href="#" wire:click.prevent="sortBy('total_elders')">
+                                            <a href="#" wire:click.prevent="sortBy('code')">
                                                 <svg class="w-3 h-3 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
-                                                    <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z"/>
+                                                    <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z" />
                                                 </svg>
                                             </a>
                                         </div>
                                     </th>
-                                    <th scope="col" class="px-10 py-3 ">
-                                        <div class="flex    items-center ">
+                                    <th scope="col" class="px-10 py-3 text-center">
+                                        <div class="flex items-center justify-center">
+                                            Número Total de Abuelos
+                                            <a href="#" wire:click.prevent="sortBy('total_elders')">
+                                                <svg class="w-3 h-3 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                                                    <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z" />
+                                                </svg>
+                                            </a>
+                                        </div>
+                                    </th>
+                                    <th scope="col" class="px-10 py-3 text-center">
+                                        <div class="flex items-center justify-center">
                                             Monto Individual
-                                            <a href="#" wire:click.prevent="sortBy('amount')"><svg class="w-3 h-3 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
-                                                <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z"/>
-                                                </svg></a>
+                                            <a href="#" wire:click.prevent="sortBy('amount')">
+                                                <svg class="w-3 h-3 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                                                    <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z" />
+                                                </svg>
+                                            </a>
                                         </div>
                                     </th>
-                                    <th scope="col" class="px-10 py-3 ">
-                                        <div class="flex    items-center ">
+                                    <th scope="col" class="px-10 py-3 text-center">
+                                        <div class="flex items-center justify-center">
                                             Monto Total
-                                            <a href="#" wire:click.prevent="sortBy('total')"><svg class="w-3 h-3 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
-                                                <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z"/>
-                                                </svg></a>
+                                            <a href="#" wire:click.prevent="sortBy('total')">
+                                                <svg class="w-3 h-3 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                                                    <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z" />
+                                                </svg>
+                                            </a>
                                         </div>
                                     </th>
-                                    <th scope="col" class="px-10 py-3 ">
-                                        <div class="flex    items-center ">
+                                    <th scope="col" class="px-10 py-3 text-center">
+                                        <div class="flex items-center justify-center">
                                             Fecha del Reporte
-                                            <a href="#" wire:click.prevent="sortBy('created_at')"><svg class="w-3 h-3 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
-                                                <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z"/>
-                                                </svg></a>
+                                            <a href="#" wire:click.prevent="sortBy('created_at')">
+                                                <svg class="w-3 h-3 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                                                    <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z" />
+                                                </svg>
+                                            </a>
                                         </div>
                                     </th>
                                 </tr>
                             </thead>
-                            <tbody class=" items-center">
-                                @forelse ( $reports as $index => $report)
-                                <tr class="bg-white border-b  flex justify-between">
-                                    <th scope="row" class=" px-6 py-4 text-gray-900 ">
-                                        <a href="{{ route('elder-program.pension.show',$report->code) }}"  class="text-blue-600 hover:underline hover:text-blue-800">
-                                            {{ $report->code }}
-                                        </a>
-                                    </th>
-                                    <td class=" px-6 py-4 text-gray-900">
-                                        {{ $report->total_elders }}
-                                    </td>
-                                    <td class=" px-6 py-4 text-gray-900">
-                                        {{ $report->amount }} Bs
-                                    </td>
-                                    <td class=" px-6 py-4 text-gray-900">
-                                        {{ $report->total }} Bs
-                                    </td>
-                                    <td class=" px-6 py-4 text-gray-900">
-                                        {{ $report->created_at->format('d/m/Y') }}
-                                    </td>
-                                </tr>
+                            <tbody>
+                                @forelse ($reports as $index => $report)
+                                    <tr class="bg-white">
+                                        <td class="px-6 py-4 text-gray-900 text-center">
+                                            <a href="{{ route('elder-program.pension.show', $report->code) }}" class="text-blue-600 hover:underline hover:text-blue-800">
+                                                {{ $report->code }}
+                                            </a>
+                                        </td>
+                                        <td class="px-6 py-4 text-gray-900 text-center">
+                                            {{ $report->total_elders }}
+                                        </td>
+                                        <td class="px-6 py-4 text-gray-900 text-center">
+                                            {{ $report->amount }}Bs.
+                                        </td>
+                                        <td class="px-6 py-4 text-gray-900 text-center">
+                                            {{ $report->total }}Bs.
+                                        </td>
+                                        <td class="px-6 py-4 text-gray-900 text-center">
+                                            {{ $report->created_at->format('d/m/Y') }}
+                                        </td>
+                                    </tr>
                                 @empty
                                     <tr>
-                                        <td class="px-6 py-4 text-center text-xl col-span-5  text-black bg-white" colspan="10">
+                                        <td class="px-6 py-4 text-center text-xl col-span-5 text-black bg-white" colspan="10">
                                             No hay Pensiones registradas.
                                         </td>
                                     </tr>
