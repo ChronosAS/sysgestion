aabfeecfca0017935e4cc8b0d932959b2ec077d3
leddinlozada223 <leddinlozada@outlook.com>
2025-05-23 11:56:45 -0400

    Update table header padding for improved layout consistency in donations index

--- resources/views/livewire/medicines/donations/index.blade.php
+++ resources/views/livewire/medicines/donations/index.blade.php
@@ -25,7 +25,7 @@
                         <table class="w-full border border-blue-700  text-center  text-white">
                             <thead class="md:text-xs 2xl:text-sm xl:text-[11px] text-sm  text-center font-semibold  text-white uppercase bg-blue-800">
                                 <tr class="">
-                                    <th scope="col" class="2xl:px-10 xl:px-9 py-3">
+                                    <th scope="col" class="2xl:px-10 xl:px-9 2xl:py-4 xl:py-3 py-2">
                                         <div class="flex   items-center ">
                                             Código
                                             <a href="#" wire:click.prevent="sortBy('code')" >
@@ -35,7 +35,7 @@
                                             </a>
                                         </div>
                                     </th>
-                                    <th scope="col" class="2xl:px-10 xl:px-9 py-3">
+                                    <th scope="col" class="2xl:px-10 xl:px-9 2xl:py-4 xl:py-3 py-2">
                                         <div class="flex   items-center">
                                             Cédula
                                             <a href="#" wire:click.prevent="sortBy('code')" >
@@ -45,7 +45,7 @@
                                             </a>
                                         </div>
                                     </th>
-                                    <th scope="col" class="2xl:px-10 xl:px-9 py-3">
+                                    <th scope="col" class="2xl:px-10 xl:px-9 2xl:py-4 xl:py-3 py-2">
                                         <div class="flex text-wrap  items-center ">
                                             Nombres y Apellidos
                                             <a href="#" wire:click.prevent="sortBy('total_elders')">
@@ -55,7 +55,7 @@
                                             </a>
                                         </div>
                                     </th>
-                                    <th scope="col" class="2xl:px-10 xl:px-9 py-3">
+                                    <th scope="col" class="2xl:px-10 xl:px-9 2xl:py-4 xl:py-3 py-2">
                                         <div class="flex    items-center ">
                                             Edad
                                             <a href="#" wire:click.prevent="sortBy('amount')">
@@ -65,7 +65,7 @@
                                             </a>
                                         </div>
                                     </th>
-                                    <th scope="col" class="2xl:px-10 xl:px-9 py-3">
+                                    <th scope="col" class="2xl:px-10 xl:px-9 2xl:py-4 xl:py-3 py-2">
                                         <div class="flex    items-center ">
                                             Teléfono
                                             <a href="#" wire:click.prevent="sortBy('total')">
@@ -75,7 +75,7 @@
                                             </a>
                                         </div>
                                     </th>
-                                    <th scope="col" class="2xl:px-10 xl:px-9 py-3">
+                                    <th scope="col" class="2xl:px-10 xl:px-9 2xl:py-4 xl:py-3 py-2">
                                         <div class="flex    items-center ">
                                             Correo Electrónico
                                             <a href="#" wire:click.prevent="sortBy('created_at')">
@@ -85,7 +85,7 @@
                                             </a>
                                         </div>
                                     </th>
-                                    <th scope="col" class="2xl:px-10 xl:px-9 py-3">
+                                    <th scope="col" class="2xl:px-10 xl:px-9 2xl:py-4 xl:py-3 py-2">
                                         <div class="flex    items-center ">
                                             Dirección
                                             <a href="#" wire:click.prevent="sortBy('created_at')">
@@ -95,7 +95,7 @@
                                             </a>
                                         </div>
                                     </th>
-                                    {{-- <th scope="col" class="2xl:px-10 xl:px-9 py-3">
+                                    {{-- <th scope="col" class="2xl:px-10 xl:px-9 2xl:py-4 xl:py-3 py-2">">
                                         <div class="flex    items-center ">
                                             Municipio
                                             <a href="#" wire:click.prevent="sortBy('created_at')"><svg class="w-3 h-3 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
@@ -103,7 +103,7 @@
                                                 </svg></a>
                                         </div>
                                     </th> --}}
-                                    <th scope="col" class="2xl:px-10 xl:px-9 py-3">
+                                    <th scope="col" class="2xl:px-10 xl:px-9 2xl:py-4 xl:py-3 py-2">
                                         <div class="flex    items-center ">
                                             Fecha de Registro
                                             <a href="#" wire:click.prevent="sortBy('created_at')">
@@ -186,7 +186,7 @@
                         <table class="w-full border border-blue-700  text-center  text-white">
                             <thead class="text-sm font-semibold  text-white uppercase bg-blue-800 ">
                                 <tr class="flex justify-around space-x-4">
-                                    <th scope="col" class="2xl:px-10 xl:px-9 py-3">
+                                    <th scope="col" class="2xl:px-10 xl:px-9 2xl:py-4 xl:py-3 py-2">
                                         <div class="flex   items-center">
                                             Código
                                             <a href="#" wire:click.prevent="sortBy('code')" ><svg class="w-3 h-3 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
@@ -194,7 +194,7 @@
                                                 </svg></a>
                                         </div>
                                     </th>
-                                    <th scope="col" class="2xl:px-10 xl:px-9 py-3">
+                                    <th scope="col" class="2xl:px-10 xl:px-9 2xl:py-4 xl:py-3 py-2">
                                         <div class="flex text-wrap  items-center ">
                                             Descripción
                                             <a href="#" wire:click.prevent="sortBy('total_elders')">
