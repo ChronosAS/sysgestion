54f97eb82fc3c01ef7a7ead2de48c7b56cda6817
Mauricio Salazar <53304693+ChronosAS@users.noreply.github.com>
2025-05-23 14:16:56 -0400

    Add Donation model reference to Donations Show component

--- app/Livewire/Medicines/Donations/Show.php
+++ app/Livewire/Medicines/Donations/Show.php
@@ -2,12 +2,15 @@
 
 namespace App\Livewire\Medicines\Donations;
 
+use App\Models\Donation;
 use Livewire\Attributes\Layout;
 use Livewire\Component;
 
 #[Layout('layouts.app')]
 class Show extends Component
 {
+    public Donation $donation;
+
     public function render()
     {
         return view('livewire.medicines.donations.show');
