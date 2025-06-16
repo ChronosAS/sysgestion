0fa25f415181cf54e92671d1ff07270d74cfa71d
leddinlozada223 <leddinlozada@outlook.com>
2025-06-16 14:49:21 -0400

    Add search button for citizen identification in medical aid request form

--- resources/views/livewire/medicines/medicine-applications/create.blade.php
+++ resources/views/livewire/medicines/medicine-applications/create.blade.php
@@ -10,6 +10,16 @@
                         <x-label for="document" value="Cédula de Identidad " class="text-black " />
                         <div class="flex items-center">
                             <x-input id="document" placeholder="e.j. 01234567" class="text-center block mt-1 w-full truncate rounded-none rounded-l-md disabled:text-slate-400" type="text" name="document" oninput="this.value = this.value.replace(/[^0-9]/g, '');" />
+                            <button class="p-[9px] mt-1 text-white bg-blue-500 hover:bg-blue-600 focus:outline-none rounded-none rounded-r-md" type="button"">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+                                </svg>
+                            </button>
+                            {{-- <button class="p-[9px] mt-1 text-white bg-red-500 hover:bg-red-600 focus:outline-none rounded-none rounded-r-md" type="button"">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+                                </svg>
+                            </button> --}}
                         </div>
                         <x-input-error class="text-xs" for="document"/>
                     </div>
