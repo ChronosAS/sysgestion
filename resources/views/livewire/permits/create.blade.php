abc9ab1f0670f2beb210b35e64140bc8e699eced
leddinlozada223 <leddinlozada@outlook.com>
2025-10-29 08:53:31 -0400

    permit create form done

--- resources/views/livewire/permits/create.blade.php
+++ resources/views/livewire/permits/create.blade.php
@@ -24,7 +24,7 @@
                         <x-input-error class="text-xs" for="document"/>
                     </div>
                     <div class="mt-5 w-full px-3 sm:w-1/6">
-                        <x-label for="dop" value="F. de Elaboración  del permiso" class="text-black" />
+                        <x-label for="dop" value="F. de Elaboración " class="text-black" />
                         <x-input id="dop" class="block mt-1 w-full " type="date" name="dop"/>
                         <x-input-error class="text-xs" for="dop"/>
                     </div>
@@ -34,7 +34,7 @@
                         <x-input-error class="text-xs" for="permit_number"/>
                     </div>
                     <div class="mt-5 w-full px-3 sm:w-1/6">
-                        <x-label for="doe" value="F. de Vencimiento del permiso" class="text-black" />
+                        <x-label for="doe" value="F. de Vencimiento " class="text-black" />
                         <x-input id="doe" class="block mt-1 w-full " type="date" name="doe"/>
                         <x-input-error class="text-xs" for="doe"/>
                     </div>
@@ -126,7 +126,7 @@
                         <x-input id="observations" class="block mt-1 w-full truncate" type="text" name="observations"/>
                         <x-input-error class="text-xs" for="observations"/>
                     </div>
-                    {{-- <div x-data="{modalIsOpen: false}" class="mt-5 w-full px-3  ">
+                    <div x-data="{modalIsOpen: false}" class="mt-5 w-full   sm:w-1/3">
                         <button x-on:click="modalIsOpen = true" type="button" class="whitespace-nowrap rounded-sm bg-black border border-black dark:border-white px-4 py-2 text-center text-sm font-medium tracking-wide text-neutral-100 transition hover:opacity-75 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black active:opacity-100 active:outline-offset-0 dark:bg-white dark:text-black dark:focus-visible:outline-white">Descripción Del evento</button>
                         <div x-cloak x-show="modalIsOpen" x-transition.opacity.duration.200ms x-trap.inert.noscroll="modalIsOpen" x-on:keydown.esc.window="modalIsOpen = false" x-on:click.self="modalIsOpen = false" class="fixed inset-0 z-30 flex items-end justify-center bg-black/20 p-4 pb-8 backdrop-blur-md sm:items-center lg:p-8" role="dialog" aria-modal="true" aria-labelledby="defaultModalTitle">
                             <!-- Modal Dialog -->     
@@ -156,8 +156,64 @@
                                 </div>
                             </div>
                         </div>
-                    </div> --}}
-                    <div class="mt-5 mx-5 container-md  text-center  flex items-center justify-center flex-wrap">
+                    </div> 
+                    <div x-data="{modalIsOpen: false}" class="mt-5 w-full   sm:w-1/3">
+                        <button x-on:click="modalIsOpen = true" type="button" class="whitespace-nowrap rounded-sm bg-black border border-black dark:border-white px-4 py-2 text-center text-sm font-medium tracking-wide text-neutral-100 transition hover:opacity-75 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black active:opacity-100 active:outline-offset-0 dark:bg-white dark:text-black dark:focus-visible:outline-white">Descripción de la Publicidad</button>
+                        <div x-cloak x-show="modalIsOpen" x-transition.opacity.duration.200ms x-trap.inert.noscroll="modalIsOpen" x-on:keydown.esc.window="modalIsOpen = false" x-on:click.self="modalIsOpen = false" class="fixed inset-0 z-30 flex items-end justify-center bg-black/20 p-4 pb-8 backdrop-blur-md sm:items-center lg:p-8" role="dialog" aria-modal="true" aria-labelledby="defaultModalTitle">
+                            <!-- Modal Dialog -->     
+                            <div x-show="modalIsOpen" x-transition:enter="transition ease-out duration-200 delay-100 motion-reduce:transition-opacity" x-transition:enter-start="opacity-0 -translate-y-8" x-transition:enter-end="opacity-100 translate-y-0" class="flex max-w-lg flex-col gap-4 overflow-hidden rounded-sm border border-neutral-300  text-neutral-600 dark:border-neutral-700 bg-blue-800 dark:text-neutral-300">             
+                                <!-- Dialog Header -->
+                                <div class="flex items-center justify-between border-b border-neutral-300 bg-neutral-50/60 p-4 dark:border-neutral-700 dark:bg-neutral-950/20">
+                                    <h3 id="defaultModalTitle" class="font-semibold tracking-wide text-neutral-900 dark:text-white">Descripción de la Publicidad</h3>
+                                    <button x-on:click="modalIsOpen = false" aria-label="close modal">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" stroke="currentColor" fill="none" stroke-width="1.4" class="w-5 h-5">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"/>
+                                        </svg>
+                                    </button>
+                                </div>
+                                <!-- Dialog Body -->
+                                <div class=" mx-auto items-center justify-center gap-4 p-4 flex flex-row flex-wrap"> 
+                                    <p>VOLANTES:</p><x-checkbox id="flyers" name="flyers"  class=""/>
+                                    <p>AFICHES:</p><x-checkbox id="posters" name="posters"  class=""/>
+                                    <p>PENDONES:</p><x-checkbox id="banner" name="banner"  class=""/>
+                                    <p>HABLADORES:</p><x-checkbox id="showcases" name="showcases"  class=""/>
+                                    <p>PANCARTAS:</p><x-checkbox id="banners" name="banners"  class=""/>
+                                    <p>CALCOMANIAS:</p><x-checkbox id="stickers" name="stickers"  class=""/>
+                                    <p class="px-4">BANDEROLAS O BANDERINES:</p><x-checkbox id="pennants" name="pennants"  class=""/>
+                                    <p>STANDS:</p><x-checkbox id="stands" name="stands"  class=""/>
+                                    <p>TOLDOS:</p><x-checkbox id="awnings" name="awnings"  class=""/>
+                                    <p>INFLABLES:</p><x-checkbox id="inflatable" name="inflatable"  class=""/>
+                                    <p>OTROS:</p><x-checkbox id="other" name="other"  class=""/>
+                                </div>
+                                <!-- Dialog Footer -->
+                                <div class="flex flex-col justify-between    bg-blue-900 p-4 sm:flex-row sm:items-center">
+                                    <button x-on:click="modalIsOpen = false" type="button" class="whitespace-nowrap rounded-md bg-red-600  px-4 py-2 text-center text-sm font-medium tracking-wide text-neutral-100 transition hover:opacity-75 active:opacity-100 active:outline-offset-0 ">Salir</button>
+                                    <button x-on:click="modalIsOpen = false" type="button" class="whitespace-nowrap rounded-md bg-green-600 px-4 py-2 text-center text-sm font-medium tracking-wide text-neutral-100 transition hover:opacity-75 active:opacity-100 active:outline-offset-0 ">Agregar</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div> 
+                    {{-- <div class=" mt-5 w-full px-3 sm:w-4/2 " >
+                        <x-label  value="Permisos Adicionales" class="text-black" />
+                        <x-table.table class="w-full ">
+                            <x-slot name="thead">
+                                <tr class="bg-blue-800 text-white ">
+                                    <x-table.th class="pb-3 text-center" >
+                                        <button class="inline-block cursor-pointer rounded-md bg-blue-900 hover:bg-blue-500 px-6 pb-2 pt-2.5 text-xs  uppercase leading-normal text-white shadow-primary-3 transition duration-150 ease-in-out hover:bg-primary-accent-300 hover:shadow-primary-2 focus:bg-primary-accent-300 focus:shadow-primary-2 focus:outline-none focus:ring-0 active:bg-primary-600 active:shadow-primary-2 motion-reduce:transition-none">Agregar</button>
+                                        
+                                    </x-table.th>
+                                </tr>
+                            </x-slot>
+                            <x-slot name="tbody">
+                                    <tr>
+                                        <td colspan="6" class="px-6 py-4 bg-white text-center text-black text-xl">
+                                            No hay Permisos adicionales agregados.
+                                        </td>
+                                    </tr>
+                            </x-slot>
+                        </x-table.table>
+                    </div>  --}}
+                    <div class="mt-5  container-md  text-center  flex items-center justify-center flex-wrap w-full px-3  sm:w-1/2">
                         <x-button-href href="{{ route('medicines.medicine-applications.index')}}" class="ms-4 mt-5 mb-5 bg-blue-900 inline-flex items-center px-4 py-2  border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-blue-500 focus:bg-blue-700 active:bg-blue-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-50 transition ease-in-out duration-150">
                             Regresar
                         </x-button-href>
