221ce0c38a04f2fa3570eb1c69aa7eeb8e3d6870
leddinlozada223 <leddinlozada@outlook.com>
2025-11-25 11:54:03 -0400

    Add delivery note and service record templates with updated styles and scripts

---
+++ resources/views/livewire/technology-and-systems/delivery-note.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nota de Entrada de Equipo</title>
+     <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+        <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body >
+    <div  class=" bg-contain relative overflow-hidden">
+        <header class="text-center right-3.5 absolute">
+        </header>
+        <main class=" justify-center items-center absolute top-28  right-[40px] mr-[40px]  block">
+        </main>
+        <footer class="text-center  flex  flex-row items-center justify-center space-x-16">
+        </footer>
+    </div>
+</body>
+</html>
